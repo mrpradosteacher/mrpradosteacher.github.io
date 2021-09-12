926885b8cc6ed9eb539224ebca42260ad3b2eed5
mrpradosteacher <mrpradosteacher@gmail.com>
2021-09-12 12:09:14 +0200

    Add axios

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="bg-blue-500">
+    {{baseUrl}}
   </div>
+  <div>v.0.2</div>
 </template>
 
 <script>
+
+import axios from 'axios';
 
 export default {
   name: 'MainPage',
   data() {
     return {
-
+      baseUrl: process.env.VUE_APP_BASE_URL,
     }
+  },
+  created(){
+      axios.get(this.baseUrl + 'data/users.json').then(response => {
+        console.log(response);
+     })
   }
 }
 </script>
